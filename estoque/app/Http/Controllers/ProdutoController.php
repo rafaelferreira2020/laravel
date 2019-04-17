f833fc3118e3ee26001ba4cb2337d51362abc519
Rafael Ferreira <rafaei.ferreira@before.com.br>
2019-04-17 00:01:18 -0400

    Criando projeto

---
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,13 @@
+<?php namespace estoque\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class ProdutoController extends Controller {
+
+    public function lista(){
+
+        $produtos = DB::select('select * from produtos');
+
+        return view('listagem')->with('produtos', $produtos);
+    }
+}
---
+++ estoque/app/Http/routes.php
@@ -0,0 +1,9 @@
+<?php
+Route::get('/produtos', 'ProdutoController@lista');
+
+Route::get('/', function(){
+
+    return '<h1>Primeira lógica com Laravel</h1>';
+});
+
+Route::get('/produtos', 'ProdutoController@lista');
